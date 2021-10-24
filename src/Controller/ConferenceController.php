70d415b4ebc2e3a0b1ba2375b346f860d858f0f2
tonakashima2025 <tonakashima2025@gmail.com>
2021-10-24 13:30:37 +0900

    Twig（インデックスページ）関連の修正

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
@@ -13,26 +15,31 @@ class ConferenceController extends AbstractController
     #[Route('/hello/{name}', name: 'homepage')]
     // public function index(): Response
     // public function index(Request $request): Response
-    public function index(string $name = ''): Response
+    // public function index(string $name = ''): Response
+    public function index(Environment $twig, ConferenceRepository $conferenceRepository): Response
     {
         // return $this->render('conference/index.html.twig', [
         //     'controller_name' => 'ConferenceController',
         // ]);
 
-        $greet = '';
+        // $greet = '';
         // if ($name = $request->query->get('hello')) {
-        if ($name) {
-            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-        }
+        // if ($name) {
+        //     $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        // }
 
-        return new Response(<<<EOF
-<html>
-    <body>
-        $greet
-        <img src="/images/under-construction.gif" />
-    </body>
-</html>
-EOF
-        );
+//         return new Response(<<<EOF
+// <html>
+//     <body>
+//         $greet
+//         <img src="/images/under-construction.gif" />
+//     </body>
+// </html>
+// EOF
+//         );
+
+        return new Response($twig->render('conference/index.html.twig', [
+                'conferences' => $conferenceRepository->findAll(),
+        ]));
     }
 }
